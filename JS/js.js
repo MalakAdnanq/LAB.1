1eb0a68ed9d50867d49f744af0aaf7b06d39198c
Malak <malak.adnan.q@gmail.com>
2021-08-03 10:04:17 +0300

    lab.1 js

---
+++ JS/js.js
@@ -0,0 +1,10 @@
+var n1= 10;
+var n2= 2;
+
+document.getElementById('num1').innerHTML = n1;
+document.getElementById('num2').innerHTML = n2;
+
+var add = n1+n2
+var sub = n1-n2
+var mul = n1*n2
+var div = n1/n2
